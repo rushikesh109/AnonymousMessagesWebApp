badc35e901334a71f33864be03e5dfdafca5ad7e
rushikesh109 <mangrulerushikesh2003@gmail.com>
2024-07-08 20:54:06 +0530

    databaseConnectivity

--- src/schemas/messageSchema.ts
+++ src/schemas/messageSchema.ts
@@ -3,6 +3,6 @@ import {z} from 'zod'
 export const signInSchema = z.object({
    content:z
    .string()
-   .min(10, {message: 'cpntent must be at least of 10 characters'})
-   .max(300,{message: 'cpntent must be no longer than 300 character'})
+   .min(10, {message: 'content must be at least of 10 characters'})
+   .max(300,{message: 'content must be no longer than 300 character'})
 })
