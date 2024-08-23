57b44b027d28450ab4a614a7f6a2182c2d8f3822
rushikesh109 <mangrulerushikesh2003@gmail.com>
2024-08-23 12:57:00 +0530

    Message Api with aggregation pipeline

--- src/app/api/send-message/route.ts
+++ src/app/api/send-message/route.ts
@@ -5,5 +5,38 @@ import { Message } from "@/model/User";
 
 export async function POST(request: Request){
     await dbConnect()
+    const { username, content} = await request.json()
+    try{
+        const user = await UserModel.findOne({username})
+        if(!user){
+            return Response.json(
+                { success: false, message: 'User not found' },
+                { status: 404 }
+              )
+        }
+// is user accepting the messages
+if(!user.isAcceptingMessage){
+    return Response.json(
+        { success: false, message: 'User is not accepting the message' },
+        { status: 403 }
+      )
+}
+
+   const newMessage = {content, createdAt: new Date()}
+   user.messages.push(newMessage as Message)
+   await user.save()
+
+    return Response.json(
+        { success: false, message: 'message sent successfully' },
+        { status: 401 }
+      )
+
+    }catch(error){
+        console.error('Error adding messages:', error);
+    return Response.json(
+      { message: 'Internal server error', success: false },
+      { status: 500 }
+    );
     
+    }
 }
