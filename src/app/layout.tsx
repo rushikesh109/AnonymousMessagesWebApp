56cce3292782f21c8eff1f87bd6690abac030b02
rushikesh109 <mangrulerushikesh2003@gmail.com>
2024-09-15 02:05:52 +0530

    user-verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/context/AuthProvider";
+import { Toaster } from "@/components/ui/toaster"
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,7 +21,9 @@ export default function RootLayout({
     <html lang="en">
     <AuthProvider>
 
-    <body className={inter.className}>{children}</body>
+    <body className={inter.className}>{children}
+    <Toaster/>  
+    </body>
     </AuthProvider>
       
     </html>
